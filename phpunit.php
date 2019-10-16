66a731d977d9dc7a72ae46c45eb74de7912f65dd
Bobby Allen <ballen@bobbyallen.me>
2019-10-16 20:09:04 +0100

    Updates to the composer packages and .gitignore, testing and cleanup on PHP 7.3.

--- phpunit.php
+++ phpunit.php
@@ -3,4 +3,9 @@
  * PHPUNIT BOOTSTRAP FILE USED TO LOAD THE COMPOSER AUTOLOADER SO WE CAN
  * UTILISE THE PACKAGE CLASSES IN THE TEST CLASSES.
  */
+
+
+// Sets the PHP precision to match our testing environment requirements.
+ini_set('precision', 10);
+
 require __DIR__ . '/vendor/autoload.php';
